32c0cd41f238c1bfc6f0d84bfa93af8b2af243bc
IlanVaks <ilanvaks@gmail.com>
2023-03-29 15:51:01 -0400

    My-first-firestore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import the tools from firebase-admin
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore"; 
+import { initializeApp, cert } from "firebase-admin/app"; // we use to connect our firebase project
+import { getFirestore } from "firebase-admin/firestore"; // we use to connect to firestone
 
 //Import our credentials from a secert file 
 import { credentials } from "./credentials.js";
@@ -12,17 +12,50 @@ initializeApp({
 
 // Connect to Firestore DB
 const db = getFirestore();
-
+// first 14 lines connecting to our database
 // Add a product to our products collection
-const candy = {
-  name: "Skittles",
-  unitPrice: 3.99,
-  size: "16 oz",
-  color: "green",
-  inventory: 144,
-  productNumber: "SK007",
+const candy2 = {
+  name: "twix",
+  unitPrice: 2.99,
+  size: "12 oz",
+  color: "gold",
+  inventory: 288,
+  productNumber: 2,
 }
 
-db.collection('products').add(candy)
-  .then(doc => console.log("added doc: " + doc.id))
+//how to add a document to Firestore
+// db.collection('products').add(candy2) // while we are waiting on the promise... (creates the promise)
+//   .then((doc) => {
+//     console.log("added doc: " + doc.id) //resolves the promise
+//     // i can be sure the inside .then() that the first pricess was completely successfully
+//   })
+//   .catch(err => console.log(err)) // if promise rejected .then doesnt work will return error because of this line
+
+
+//How to update a document in Firestore:
+db.collection('products').doc('b8lAwPczsHuWxQcR7E2W').update({
+  inventory: 555,
+  customerFavorite: true 
+}) 
+
+
+// how to read a document from Firestore: (CRUD)
+
+// db.collection('products').doc('b8lAwPczsHuWxQcR7E2W').get() 
+//THIS is how u delete a document even though you 
+//dont delete much 
+
+ db.collection('products').doc('b8lAwPczsHuWxQcR7E2W').get() 
+  .then(doc => {
+    console.log(doc.data())
+  })
+  .catch(err => console.log(err))
+
+  //how to get a whole collection:
+
+db.collection('products').get()
+  .then(collection => {
+    const productList = collection.docs.map(doc => ({...doc.data(), id: doc.id }))
+    console.table(productList); // can be console.log or .table
+  })
   .catch(err => console.log(err)) 
